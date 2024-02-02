96a4a73ec034516df7b82cbc58c92f2fb526413e
David Rimar <david.rimar@digital.education.gov.uk>
2024-02-02 17:35:30 +0000

    EC-285 school name validation

--- src/SFA.DAS.EarlyConnect.Web/ViewModels/SchoolNameEditViewModel.cs
+++ src/SFA.DAS.EarlyConnect.Web/ViewModels/SchoolNameEditViewModel.cs
@@ -1,10 +1,12 @@
 using SFA.DAS.EarlyConnect.Web.Infrastructure;
 using SFA.DAS.EarlyConnect.Web.RouteModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SFA.DAS.EarlyConnect.Web.ViewModels
 {
     public class SchoolNameEditViewModel : TriageRouteModel
     {
+        [RegularExpression(@"^[\w\s&-]+$", ErrorMessage = "Invalid School Name")]
         public string? SchoolName { get; set; }
         public string? BacklinkRoute =>
             IsCheck && IsOther ? RouteNames.CheckYourAnswers_Get :
