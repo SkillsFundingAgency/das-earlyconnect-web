36554f344cb530631ac0359c6f8f2ea6b1688c39
Karen Hanna <karen@laveryhanna.com>
2024-02-27 13:25:10 +0000

    integrated libphonenumber-csharp for telephone number validation

--- src/SFA.DAS.EarlyConnect.Web/Validations/TelephoneModelValidator.cs
+++ src/SFA.DAS.EarlyConnect.Web/Validations/TelephoneModelValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using PhoneNumbers;
 using SFA.DAS.EarlyConnect.Web.ViewModels;
 
 namespace SFA.DAS.EarlyConnect.Web.Validations
@@ -6,8 +7,16 @@ namespace SFA.DAS.EarlyConnect.Web.Validations
     public static class Extensions
     {
         public static IRuleBuilderOptions<T, string> MatchPhoneNumber<T>(this IRuleBuilder<T, string> rule)
-            => rule.Matches(@"^[\d+]+$").WithMessage("Invalid phone number")
+            => rule
+            .Must(BeValidPhoneNumber).WithMessage("Invalid phone number.")
             .MaximumLength(13).WithMessage("Phone number too long");
+
+        private static bool BeValidPhoneNumber(string phoneNumber)
+        {
+            var phoneUtil = PhoneNumberUtil.GetInstance();
+            var parsedNumber = phoneUtil.Parse(phoneNumber, "GB");
+            return phoneUtil.IsValidNumber(parsedNumber);
+        }
     }
 
     public class TelephoneModelValidator : AbstractValidator<TelephoneEditViewModel>
@@ -16,5 +25,7 @@ namespace SFA.DAS.EarlyConnect.Web.Validations
         {
             RuleFor(x => x.Telephone).MatchPhoneNumber();
         }
+
+        
     }
 }
