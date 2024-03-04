e2018a36ea1e47ae6625c0250b139bed1d2747dd
Karen Hanna <karen@laveryhanna.com>
2024-03-04 16:32:14 +0000

    updated error message

--- src/SFA.DAS.EarlyConnect.Web/Validations/TelephoneModelValidator.cs
+++ src/SFA.DAS.EarlyConnect.Web/Validations/TelephoneModelValidator.cs
@@ -5,11 +5,10 @@ using SFA.DAS.EarlyConnect.Web.ViewModels;
 namespace SFA.DAS.EarlyConnect.Web.Validations
 {
     public static class Extensions
-    {
+    {     
         public static IRuleBuilderOptions<T, string> MatchPhoneNumber<T>(this IRuleBuilder<T, string> rule)
-            => rule
-            .Must(BeValidPhoneNumber).WithMessage("Invalid phone number.")
-            .MaximumLength(13).WithMessage("Phone number too long");
+        => rule
+        .Must(BeValidPhoneNumber).WithMessage("Enter a telephone number, like 01632 960 001, 07700 900 982 or + 44 808 157 0192");
 
         private static bool BeValidPhoneNumber(string phoneNumber)
         {
@@ -21,7 +20,7 @@ namespace SFA.DAS.EarlyConnect.Web.Validations
             try
             {
                 var phoneUtil = PhoneNumberUtil.GetInstance();
-                var parsedNumber = phoneUtil.Parse(phoneNumber, "GB");
+                var parsedNumber = phoneUtil.Parse(phoneNumber.Trim(), "GB");
                 return phoneUtil.IsValidNumber(parsedNumber);
             }
             catch 
